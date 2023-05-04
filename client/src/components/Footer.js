3744acf5d38ff7bb0f7b03213e01745a3949b6c1
Ryan Maloney <rypmaloney@gmail.com>
2023-05-04 15:46:41 -0400

    Remove socket size.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,21 +5,20 @@ import Clock from './Clock';
 export default function Footer(props) {
     const { socketSize, stanza, intervalDuration, intervalStartTime } = props;
     return (
-        <div className='inline-flex  fixed  right-0 md:bottom-0 md:left-0  px-2 md:py-2  space-x-8 text-md'>
-            <p>
+        <div className='inline-flex fixed  right-0 md:bottom-0 md:left-0  px-2 md:py-2  space-x-8 text-md'>
+            <p className='my-auto'>
                 <span className='text-xs'>||</span> <Link to='/'>home </Link>{' '}
                 <span className='text-xs'> ||</span>{' '}
                 <Link to='/about'>about </Link>{' '}
                 <span className='text-xs'>||</span>{' '}
             </p>
-            <div className='hidden md:inline-flex text-stone-500 space-x-3 text-sm m-auto'>
+            <div className='hidden md:inline-flex text-stone-400 space-x-4 text-sm m-auto'>
                 <Clock
                     stanza={stanza}
                     intervalDuration={intervalDuration}
                     intervalStartTime={intervalStartTime}
                 />
-                <p>current visitors: {socketSize}</p>
-                <p className='text-xs align-bottom m-auto'>
+                <p className='text-xs align-bottom my-auto'>
                     Hover over image to see caption.
                 </p>
             </div>
